b3fca25112e39ef8db1f46c6819aeec2e63549b3
“sundung” <sun1358742@163.com>
2021-08-04 20:12:37 +0800

    增加动画

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,11 +1,16 @@
 // pages/personal/personal.js
+let startY = 0; // 手指起始的坐标
+let moveY = 0; // 手指移动的坐标
+let moveDistance = 0; // 手指移动的距离
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    coverTransform: 'translateY(200)rpx', // 下拉动画的移动距离
+    coveTransition:''
   },
 
   /**
@@ -14,7 +19,36 @@ Page({
   onLoad: function (options) {
 
   },
-
+  handleTouchStart(event){
+    // 获取起始坐标
+     startY = event.touches[0].clientY;
+  },
+  handleTouchMove(event){
+    // 获取移动的坐标
+     moveY = event.touches[0].clientY;
+    // 获取手指的移动距离
+    moveDistance = moveY - startY;
+    // 禁止上划
+    if(moveDistance <= 0){
+      return;
+    }
+    // 滑动的最大距离为 80 
+    if(moveDistance >= 80){
+      moveDistance = 80;
+    }
+    // 动态更新手指的移动距离
+    this.setData({
+      coverTransform: `translateY(${moveDistance}rpx)`
+    })
+    console.log(moveDistance)
+  },
+  handleTouchEnd(event){
+    // 动态更新coverTransform的状态值
+    this.setData({
+      coverTransform: `translateY(0rpx)`,
+      coveTransition: 'transform 1s linear'
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
